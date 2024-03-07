0b32b713c26b70a5eb13210ab7af9634a56e2bae
roopxx <127219908+roopxx@users.noreply.github.com>
2024-03-07 13:23:11 +0530

    Set up Router components to load different routes

---
+++ src/components/Router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import App from "../App";
+import About from "./About";
+import Contact from "./Contact";
+import Store from "./Store";
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        { path: "store", element: <Store /> },
+        { path: "about", element: <About /> },
+        { path: "contact", element: <Contact /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
