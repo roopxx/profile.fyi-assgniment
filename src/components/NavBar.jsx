c4e81af1d33af9c9a909e6d59c15ae7fc8f8b082
roopxx <127219908+roopxx@users.noreply.github.com>
2024-03-16 22:56:53 +0530

    Make NavBar mobile responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,118 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+  const [show, setShow] = useState(true);
   return (
     <>
-      <nav className="flex w-full items-center justify-between p-8 text-3xl text-black">
-        <h1>WallWonder</h1>
-        <ul className="flex justify-end gap-20">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "text-red-700" : "")}
-          >
-            HOME
-          </NavLink>
-          <NavLink
-            to="/store"
-            className={({ isActive }) => (isActive ? "text-red-700" : "")}
-          >
-            STORE
-          </NavLink>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? "text-red-700" : "")}
-          >
-            ABOUT US
-          </NavLink>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) => (isActive ? "text-red-700" : "")}
-          >
-            CONTACT
-          </NavLink>
-        </ul>
-      </nav>
+      <header className="w-full px-5 py-6 text-black shadow md:px-10">
+        <nav className="md:flex md:items-center md:justify-between">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-bold tracking-wider md:text-4xl">
+              <span className="text-red-700">W</span>all
+              <span className="text-red-700">W</span>onder
+            </h2>
+            <button
+              className="md:hidden"
+              onClick={() => {
+                setShow(!show);
+                document.querySelector(".menu").classList.toggle("hidden");
+              }}
+            >
+              {show ? (
+                <div className="m-0 p-0 transition-transform hover:duration-300 hover:ease-linear hover:rotate-180">
+                  <svg
+                    fill="#000000"
+                    width="28px"
+                    height="28px"
+                    viewBox="0 -2 28 28"
+                    xmlns="http://www.w3.org/2000/svg"
+                    stroke="#000000"
+                    strokeWidth="0.00028"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      stroke="#CCCCCC"
+                      strokeWidth="0.11200000000000002"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path d="m0 0h27.65v5.219h-27.65z"></path>
+                      <path d="m0 9.39h27.65v5.219h-27.65z"></path>
+                      <path d="m0 18.781h27.65v5.219h-27.65z"></path>
+                    </g>
+                  </svg>
+                </div>
+              ) : (
+                <div className="m-0 p-0 transition-transform hover:duration-300 hover:ease-linear hover:rotate-180">
+                  <svg
+                    fill="#000000"
+                    width="28px"
+                    height="28px"
+                    viewBox="0 0 16.00 16.00"
+                    xmlns="http://www.w3.org/2000/svg"
+                    stroke="#000000"
+                    strokeWidth="0.8"
+                  >
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      stroke="#CCCCCC"
+                      strokeWidth="0.8"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <path
+                        d="M0 14.545L1.455 16 8 9.455 14.545 16 16 14.545 9.455 8 16 1.455 14.545 0 8 6.545 1.455 0 0 1.455 6.545 8z"
+                        fillRule="evenodd"
+                      ></path>
+                    </g>
+                  </svg>
+                </div>
+              )}
+            </button>
+          </div>
+          <div>
+            <ul className="menu hidden gap-8 space-y-2 text-right text-xl md:flex md:space-y-0 md:text-2xl">
+              <li className="mt-2 md:mt-0">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) => (isActive ? "text-red-700" : "")}
+                >
+                  HOME
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/store"
+                  className={({ isActive }) => (isActive ? "text-red-700" : "")}
+                >
+                  STORE
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) => (isActive ? "text-red-700" : "")}
+                >
+                  ABOUT US
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) => (isActive ? "text-red-700" : "")}
+                >
+                  CONTACT
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </>
   );
 }
