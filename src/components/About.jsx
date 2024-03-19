d052b70751601a3b0839fea33efb78a5f17d9167
roopxx <127219908+roopxx@users.noreply.github.com>
2024-03-19 14:00:33 +0530

    Add responsiveness in About and Contact page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,34 +4,34 @@ function About() {
   return (
     <>
       <NavBar />
-      <div className="mx-20">
+      <div className="mx-4 mb-10 md:mx-10">
         <div className="mt-12 space-y-10">
-          <h1 className="text-center text-6xl">
+          <h1 className="text-center text-3xl md:text-6xl">
             About <span className="font-bold text-red-700">Wall</span>
             <span className="font-bold">Wonder</span>.
           </h1>
-          <p className="text-center text-4xl">
+          <p className="text-center text-xl md:text-4xl">
             At Wall Wonder, we are passionate about helping you transform your
             living spaces into vibrant, captivating environments.
           </p>
-          <p className="text-center text-4xl">
+          <p className="text-center text-xl md:text-4xl">
             Our journey began with a simple idea: to provide an extensive
             collection of high-quality posters that cater to diverse tastes and
             preferences.
           </p>
-          <p className="text-center text-4xl">
+          <p className="text-center text-xl md:text-4xl">
             We believe that every wall has the potential to tell a story and
             evoke emotions. Whether you&apos;re a fan of anime, comics, or
             classic art, we offer a curated selection of posters that speak to
             your passions.
           </p>
-          <p className="text-center text-4xl">
+          <p className="text-center text-xl md:text-4xl">
             We are dedicated to ensuring that your experience with Wall Wonder
             is nothing short of exceptional. From browsing our catalog to
             receiving your order, we strive for excellence every step of the
             way.
           </p>
-          <p className="text-center text-4xl">
+          <p className="text-center text-xl md:text-4xl">
             Thank you for choosing Wall Wonder as your ultimate destination for
             poster paradise. We look forward to helping you turn your walls into
             works of art!
